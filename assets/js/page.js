794db7d465cc8eff2bfde89e2c3b3c7ac4d200dd
EdwardFuchs <EdwardFuchs@mail.ru>
2020-11-07 02:49:52 +0300

    1.0.2

--- assets/js/page.js
+++ assets/js/page.js
@@ -183,7 +183,7 @@ function SearchAlgoritm() {
     li = ul.getElementsByTagName("li");
     for (i = 0; i < li.length; i++) {
         a = li[i];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        if (a["textContent"].toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
